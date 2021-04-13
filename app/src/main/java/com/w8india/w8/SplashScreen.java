a6cb9865320de02865ec6627a55504a1e55e658b
suhailroushan <suhailroushan13@gmail.com>
2021-04-13 19:55:11 +0530

    Bus Select

--- app/src/main/java/com/w8india/w8/SplashScreen.java
+++ app/src/main/java/com/w8india/w8/SplashScreen.java
@@ -1,16 +1,22 @@
 package com.w8india.w8;
 
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimatedImageDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
 
 public class SplashScreen extends AppCompatActivity {
 
 
 
     Button connectwithdriver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,8 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         connectwithdriver=findViewById(R.id.connectwithdriver);
         connectwithdriver.setOnClickListener(v -> Selection());
+
+
 
 
 
--- app/src/main/java/com/w8india/w8/Student_OTP.java
+++ app/src/main/java/com/w8india/w8/Student_OTP.java
@@ -2,13 +2,27 @@ package com.w8india.w8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Student_OTP extends AppCompatActivity {
+
+    Button getotp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student__o_t_p);
+
+        getotp=findViewById(R.id.getotp);
+        getotp.setOnClickListener(v -> Select_Bus());
+
+
+    }
+
+    public void Select_Bus(){
+        Intent intent = new Intent(this, Select_Bus.class);
+        startActivity(intent);
     }
 }
