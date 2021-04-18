173b5768fc5bf85da7546e28fe76cde626eea8ad
suhailroushan <suhailroushan13@gmail.com>
2021-04-19 01:09:57 +0530

    Suhail done here

--- app/src/main/java/com/w8india/w8/Share.java
+++ app/src/main/java/com/w8india/w8/Share.java
@@ -10,6 +10,7 @@ public class Share extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+        getSupportActionBar().hide();
 
     }
 }
