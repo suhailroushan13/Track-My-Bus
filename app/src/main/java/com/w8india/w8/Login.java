614472e35cfb0df344ae84d6032f6630de7ef376
suhailroushan <suhailroushan13@gmail.com>
2021-04-24 23:30:10 +0530

    Suhail Map Half Done

--- app/src/main/java/com/w8india/w8/Login.java
+++ app/src/main/java/com/w8india/w8/Login.java
@@ -33,7 +33,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_google_login);
         firebaseAuth = FirebaseAuth.getInstance();
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
